5dddc9d5127ba97987daf40ea07236aac7a376d2
kerr <bottom_in2132@hotmail.com>
2024-07-13 14:52:37 +0800

    1.新增猜對次數

--- game.py
+++ game.py
@@ -6,6 +6,12 @@ for i in range(5):
     y = int(input("請猜一個數字:"))
     if x == y:
         print("猜對了!")
+        print(f"這是第{i+1}次猜對")
         break
     else:
-        print("猜錯了!!")
+        if x > y:
+            print("猜高一點!!")
+        else:
+            print("猜低一點!!")
+if x != y:
+    print(f"答案為:{x}")
